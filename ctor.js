be0d2b0816b3f64de91bd76609f9a9d3b89299bf
Norman Xu <homyu.shinn@gmail.com>
2014-05-19 17:24:38 +0100

    Release 0.0.5

--- ctor.js
+++ ctor.js
@@ -1,9 +1,85 @@
-//>>excludeStart("release", pragmas.release);
-if (typeof define !== 'function' && typeof module != 'undefined') {
-    var define = require('amdefine')(module);
-}
-//>>excludeEnd("release");
-define(function (Instance) {
+(function() { 
+var global = new Function('return this')();var myDefine = (function(factory){ var ret = factory();typeof module != 'undefined' && (module.exports = ret);global.define && global.define(function(){return ret;});global.ctor = ret; });var require, define;
+(function (undef) {
+    var mod = {}, g = this;
+    function resolvePath(base, relative){
+        var ret, upCount = 0, l;
+
+        base = base.split('/');
+        relative = relative.split('/');
+        if ( relative[0] == '.' || relative[0] == '..' ) {
+            base.pop();
+            ret = base.concat(relative);
+        }
+        else {
+            ret = relative;
+        }
+
+        for(l = ret.length ; l--; ){
+            if ( ret[l] == '.' ) {
+                ret.splice( l, 1 );
+            }
+            else if ( ret[l] == '..' ) {
+                upCount++;
+            }
+            else {
+                if ( upCount > 0 ) {
+                    ret.splice( l, 2 );
+                    upCount--;
+                }
+            }
+        }
+        return ret.join('/');
+    }
+    define = function( id, deps, factory ){
+        mod[id] = {
+            p: id,
+            d: deps,
+            f: factory
+        };
+    };
+    define.amd = true;
+    require = function(deps, factory){
+        var module = this;
+        var resolved = [], cur, relative, absolute;
+
+        if ( module == null || module === g ) {
+            module = { p: '_NE_' };
+        }
+
+        if ( typeof deps == 'string' && factory == null ) {
+            deps = [deps];
+        }
+
+        for(var i = 0; i < deps.length; i++) {
+            relative = deps[i];
+            absolute = resolvePath( module.p, relative );
+            if ( absolute == "require" ) {
+                cur = {
+                    p: '_NE_',
+                    d: [],
+                    f: function(){ return require }
+                };
+            }
+            else {
+                cur = mod[absolute];
+            }
+            if ( !cur ) {throw "module not found"}
+            resolved.push( require.call( cur, cur.d, cur.f ) );
+        }
+
+        resolved.push(require, {});
+        if ( factory ) {
+            return factory.apply(g, resolved);
+        }
+        else {
+            return resolved[0];
+        }
+    };
+}());
+define("../lib/amdshim/amdshim", function(){});
+
+define('ctor',['require'],function (Instance) {
 
     function Empty ( ) { }
 
@@ -107,3 +183,5 @@ define(function (Instance) {
 
     return ctor;
 });
+myDefine(function() { return require('ctor'); }); 
+}());
